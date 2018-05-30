2f82aebd421d176a7a8f4f9ed791eab1f609e978
Michael Ran <michaelran16@gmail.com>
2018-05-30 22:10:47 +0800

    fix heroku deployment

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const upload = multer({
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.load({
-  path: 'config-fb-mbp.env'
+  // path: 'config-fb-mbp.env'
+  path: '.env.example'
 });
 
 /**
